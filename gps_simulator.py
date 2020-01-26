a0fdddd729be774e4a3885c37e0b7cf8a4af3937
Maciej Ziaja <maciejzjg@gmail.com>
2020-01-26 09:26:30 +0100

    Draft function definitions

--- gps_simulator.py
+++ gps_simulator.py
@@ -3,6 +3,7 @@
 import argparse
 #import serial
 from scipy.interpolate import interp1d
+import csv
 
 APP_DESCRIPTION = 'GPS simulator, that takes flight path in a form of csv file \
                    and outputs NMEA messages accordingly on a serial port.'
@@ -16,4 +17,16 @@ parser.add_argument('-t', '--tty', action='store', default='ttyUSB0',
                     help='filename for desired usb tty output')
 
 args = parser.parse_args()
+
+def csv_flight_path_to_lists(csv_reader):
+    pass
+    
+def time_match_baudrate(time, baudrate):
+    pass
+    
+def interpolate_coordinates(time, latitude, longitude):
+    pass
+    
+def simulate(time, latitude, longitude, baudrate, tty):
+    pass
 			
